a1169bc01271385e9d732948b49e041d97384abf
Alexis <alexis.gressier62@gmail.com>
2018-07-22 17:34:49 +0200

    show my posts

---
+++ Portfolio/src/Controller/ViewPostsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AddPost;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ViewPostsController extends Controller
+{
+    /**
+     * @Route("/view/posts", name="view_posts")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showPosts()
+    {
+
+        $repository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository(AddPost::class);
+        $listArticles = $repository->findAll();
+
+            return $this->render('view_posts/index.html.twig', array(
+                'listeArticles' => $listArticles
+            ));
+    }
+}
